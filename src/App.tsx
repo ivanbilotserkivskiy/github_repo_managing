1148427dc51ec3b9a4411d6f2995187effdb033a
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2024-04-13 14:38:47 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { RecoilRoot } from 'recoil';
+
+function App() {
+  return (
+    <RecoilRoot>  
+      <>
+        <h1>Hello World</h1>
+      </>
+    </RecoilRoot>
+  )
+}
+
+export default App
